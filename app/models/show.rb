d7a134bdf36d00e92a06d78902ca09b425abd3cc
Warren Yao <warrenyca905@gmail.com>
2019-08-17 21:49:57 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,26 @@ class Show < ActiveRecord::Base
     self.maximum(:rating)
   end
   
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+  
   def self.most_popular_show
     self.where("rating = ?", self.highest_rating).first
   end
+  
+  def self.least_popular_show
+    self.where("rating = ?", self.lowest_rating).first
+  end
+  
+  def ratings_sum
+    self.sum(:rating)
+  end
+  
+  def popular_shows
+    self.where("rating > ?", 5)
+  end
+  
+  def shows_by_alphabetical_order
+    
 end
